8b3027dcf39fe89d081f4061e9a6a36869038abb
Diogo Sequeira <diogosequeira@hotmail.fr>
2021-02-16 15:42:29 +0000

    finished class TokenService

--- Services/Implementations/TokenService.cs
+++ Services/Implementations/TokenService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using loginjwt.Configurations;
 using Microsoft.IdentityModel.Tokens;
@@ -41,12 +42,35 @@ namespace loginjwt.Services.Implementations
 
         public string GenerateRefreshToken()
         {
-            throw new System.NotImplementedException();
+            var randomNumber = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomNumber);
+                return Convert.ToBase64String(randomNumber);
+            }
+
+            ;
         }
 
         public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
         {
-            throw new System.NotImplementedException();
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.Secret)),
+                ValidateLifetime = false,
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken;
+
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken);
+            var jwtSecurityToken = securityToken as JwtSecurityToken;
+            if (jwtSecurityToken == null ||
+                !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCulture))
+                throw new SecurityTokenException("Invalid Token");
+            return principal;
         }
     }
 }
