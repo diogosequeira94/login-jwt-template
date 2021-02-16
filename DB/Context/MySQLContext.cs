0c15a44cbbbd32085a4c780613e03079e4ab69db
Diogo Sequeira <diogosequeira@hotmail.fr>
2021-02-16 14:47:01 +0000

    adds mySqlContext

---
+++ DB/Context/MySQLContext.cs
@@ -0,0 +1,18 @@
+﻿using loginjwt.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace loginjwt.DB.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext()
+        {
+        }
+
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
