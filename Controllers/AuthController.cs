68b0b5ee7334a7396fe6bba5f1112a6d85b859d7
Diogo Sequeira <diogosequeira@hotmail.fr>
2021-02-16 16:45:17 +0000

    adds authcontroller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using loginjwt.Business;
+using loginjwt.Model.ValueObjects;
+using Microsoft.AspNetCore.Mvc;
+
+namespace loginjwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private ILoginBusiness _loginBusiness;
+
+        public AuthController(ILoginBusiness loginBusiness)
+        {
+            _loginBusiness = loginBusiness;
+        }
+
+        [HttpPost]
+        [Route("signin")]
+        public IActionResult SignIn([FromBody] UserVO user)
+        {
+            if (user == null) return BadRequest("Invalid client request");
+            var token = _loginBusiness.ValidateCredentials(user);
+            if (token == null) return Unauthorized();
+            return Ok(token);
+        }
+    }
+}
